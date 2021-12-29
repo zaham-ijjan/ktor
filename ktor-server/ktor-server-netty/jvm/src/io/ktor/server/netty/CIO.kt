d026a7c43d02e4c59aadacdf706468b3414376b1
lolilofit <usovadasha@myttk.ru>
2021-12-29 11:24:41 +0300

    Remove unused import

--- ktor-server/ktor-server-netty/jvm/src/io/ktor/server/netty/CIO.kt
+++ ktor-server/ktor-server-netty/jvm/src/io/ktor/server/netty/CIO.kt
@@ -5,8 +5,6 @@
 package io.ktor.server.netty
 
 import io.ktor.util.cio.*
-import io.ktor.utils.io.*
-import io.netty.buffer.*
 import io.netty.channel.*
 import io.netty.util.concurrent.*
 import io.netty.util.concurrent.Future
